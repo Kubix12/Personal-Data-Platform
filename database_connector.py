2711bc003e34822373f311db028f33257dadcaec
Jakub <banaszek.jakub1@gmail.com>
2024-03-04 21:01:35 +0100

    add connection with job_database

--- database_connector.py
+++ database_connector.py
@@ -1,6 +1,8 @@
 from database import Database
+from job_database import JobDatabase
 
 
+# -- connector with database.py --
 def connection():
     name = 'demo'
     user = 'postgres'
@@ -18,3 +20,17 @@ def authenticate(username, password):
         return True
     else:
         return False
+
+
+# -- connector with job_database.py --
+
+def connection_job():
+    name = 'demo'
+    user = 'postgres'
+    password = '123'
+    host = 'localhost'
+    port = '5432'
+    table = 'job_application'
+
+    db_job = JobDatabase(name, user, password, host, port, table)
+    return db_job
