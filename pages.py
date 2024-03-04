9f0737fd988f9f77aaccb44bc9fd2ebc7d694155
Jakub <banaszek.jakub1@gmail.com>
2024-03-04 21:00:50 +0100

    added job_application, calendar and to-do list function

--- pages.py
+++ pages.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import database_connector
 from database import Database
+from job_database import JobDatabase
+import pandas as pd
 
 
 def main():
@@ -43,12 +45,35 @@ def sign_up_page():
             st.success("Username already exist. Please try another username")
 
 
+def job_application():
+    st.title('Job application App')
+    st.subheader("Save Data:")
+    job_title = st.text_input("Job title:")
+    company_name = st.text_input("Company name:")
+    notes = st.text_input("Important notes:")
+    if st.button("Save"):
+        database_connector.connection_job().save_data(job_title, company_name, notes)
+        st.success("Data saved successfully!")
+    sorted_data = database_connector.connection_job().get_data_sorted_by_date()
+    st.write(pd.DataFrame(sorted_data, columns=["Date", "Job Title", "Company Name", "Notes"]))
+
+
+def calendar():
+    st.title('Calendar')
+
+
+def to_do_list():
+    st.title('To do list')
+
+
 def dashboard():
     st.title("Dashboard")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        if st.button("To-Do list"):
-            todo_list_page()
-    with right_column:
-        if st.button("Calendar"):
-            calendar()
+    page = st.selectbox('Choose one', ['Job application', 'Calendar', 'To do list'])
+    if st.button("Logout"):
+        st.session_state.is_authenticated = False
+    if page == 'Job application':
+        job_application()
+    elif page == 'Calendar':
+        calendar()
+    elif page == 'To do list':
+        to_do_list()
